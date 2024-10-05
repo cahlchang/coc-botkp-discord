19dec53c7b3829fb9d8cdb9f34939e09aa8a0cf4
nao morikawa <kahlua.dane@gmail.com>
2024-10-06 00:34:31 +0900

    このテストはdynamoDB化に従い不要になるのでコメントアウト

--- lambda/coc-bot-kp/tests/yig/util/data_test.py
+++ lambda/coc-bot-kp/tests/yig/util/data_test.py
@@ -32,57 +32,57 @@ def mock_s3_client():
     return mock_client
 
 
-@patch("yig.util.data.boto3.client")
-def test_write_user_data_success(mock_boto_client, mock_s3_client):
-    mock_boto_client.return_value = mock_s3_client
+# @patch("yig.util.data.boto3.client")
+# def test_write_user_data_success(mock_boto_client, mock_s3_client):
+#     mock_boto_client.return_value = mock_s3_client
 
-    guild_id = "guild123"
-    user_id = "user456"
-    filename = "test.txt"
-    content = {'test': 'Test Value'}
+#     guild_id = "guild123"
+#     user_id = "user456"
+#     filename = "test.txt"
+#     content = {'test': 'Test Value'}
 
-    success = write_user_data(guild_id, user_id, filename, content)
-    assert success is True
-
-
-@patch("yig.util.data.boto3.client")
-def test_write_user_data_failure(mock_boto_client, mock_s3_client):
-    mock_boto_client.return_value = mock_s3_client
-    mock_s3_client.put_object.side_effect = Exception("S3 Error")
-
-    guild_id = "guild123"
-    user_id = "user456"
-    filename = "test.txt"
-    content = "Hello, world!"
-
-    with pytest.raises(Exception) as exc_info:
-        write_user_data(guild_id, user_id, filename, content)
-
-    assert str(exc_info.value) == "S3 Error"
+#     success = write_user_data(guild_id, user_id, filename, content)
+#     assert success is True
 
 
-@patch("yig.util.data.boto3.client")
-def test_read_user_data_success(mock_boto_client, mock_s3_client):
-    mock_boto_client.return_value = mock_s3_client
+# @patch("yig.util.data.boto3.client")
+# def test_write_user_data_failure(mock_boto_client, mock_s3_client):
+#     mock_boto_client.return_value = mock_s3_client
+#     mock_s3_client.put_object.side_effect = Exception("S3 Error")
 
-    guild_id = "guild123"
-    user_id = "user456"
-    filename = "test.txt"
+#     guild_id = "guild123"
+#     user_id = "user456"
+#     filename = "test.txt"
+#     content = "Hello, world!"
 
-    result = read_user_data(guild_id, user_id, filename)
-    assert result == {'test': 'Test Value'}
+#     with pytest.raises(Exception) as exc_info:
+#         write_user_data(guild_id, user_id, filename, content)
+
+#     assert str(exc_info.value) == "S3 Error"
 
 
-@patch("yig.util.data.boto3.client")
-def test_read_user_data_failure(mock_boto_client, mock_s3_client):
-    mock_boto_client.return_value = mock_s3_client
-    mock_s3_client.get_object.side_effect = ClientError(
-        {"Error": {"Code": "NoSuchKey"}}, "GetObject"
-    )
+# @patch("yig.util.data.boto3.client")
+# def test_read_user_data_success(mock_boto_client, mock_s3_client):
+#     mock_boto_client.return_value = mock_s3_client
 
-    try:
-        read_user_data("guild123", "user456", "test.txt")
-    except ClientError as e:
-        assert e.response["Error"]["Code"] == "NoSuchKey"
+#     guild_id = "guild123"
+#     user_id = "user456"
+#     filename = "test.txt"
 
-    mock_s3_client.get_object.assert_called_once()
+#     result = read_user_data(guild_id, user_id, filename)
+#     assert result == {'test': 'Test Value'}
+
+
+# @patch("yig.util.data.boto3.client")
+# def test_read_user_data_failure(mock_boto_client, mock_s3_client):
+#     mock_boto_client.return_value = mock_s3_client
+#     mock_s3_client.get_object.side_effect = ClientError(
+#         {"Error": {"Code": "NoSuchKey"}}, "GetObject"
+#     )
+
+#     try:
+#         read_user_data("guild123", "user456", "test.txt")
+#     except ClientError as e:
+#         assert e.response["Error"]["Code"] == "NoSuchKey"
+
+#     mock_s3_client.get_object.assert_called_once()
